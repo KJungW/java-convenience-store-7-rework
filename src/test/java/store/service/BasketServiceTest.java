2d0761072e88aa6b93be6f9df903ffcdf8a7c953
KJungW <sagakkb0707@naver.com>
2024-11-17 14:10:16 +0900

    test(basket): 장바구니 아이템 추가 테스트 추가
    추가 내용
    - 장바구니 아이템을 정상적으로 추가할 수 있는지 테스트
    - 상품의 수량이 부족할 경우 장바구니에 아이템을
      추가할 수 없는 것을 테스트

---
+++ src/test/java/store/service/BasketServiceTest.java
@@ -0,0 +1,61 @@
+package store.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.constant.exception_message.ServiceExceptionMessage;
+import store.domain.BasketItem;
+import store.domain.Product;
+import store.repository.BasketItemRepository;
+import store.repository.ProductRepository;
+
+class BasketServiceTest {
+
+    private BasketService basketService;
+    private ProductRepository productRepository;
+    private BasketItemRepository basketItemRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        productRepository = new ProductRepository();
+        basketItemRepository = new BasketItemRepository();
+        basketService = new BasketService(basketItemRepository, productRepository);
+    }
+
+    @DisplayName("장바구니에 아이템을 추가할 수 있다")
+    @Test
+    void 장바구니에_아이템을_추가할_수_있다() {
+        productRepository.add(new Product("콜라", 3000, 10, "탄산2+1"));
+        productRepository.add(new Product("사이다", 2000, 10));
+        List<BasketItem> basketItems = List.of(
+                new BasketItem("콜라", 10),
+                new BasketItem("사이다", 5));
+
+        basketService.addBasketItems(basketItems);
+
+        checkAddedBasketItems(basketItems);
+    }
+
+    void checkAddedBasketItems(List<BasketItem> expectedAddedBasketItems) {
+        List<BasketItem> actualAddedBasketItems = basketItemRepository.findAll();
+        assertThat(actualAddedBasketItems)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(expectedAddedBasketItems);
+    }
+
+    @DisplayName("상품의 수량이 부족하면 장바구니에 아이템을 추가할 수 없다")
+    @Test
+    void 상품의_수량이_부족하면_장바구니에_아이템을_추가할_수_없다() {
+        productRepository.add(new Product("콜라", 3000, 10, "탄산2+1"));
+        List<BasketItem> basketItems = List.of(new BasketItem("콜라", 15));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> basketService.addBasketItems(basketItems))
+                .withMessage(ServiceExceptionMessage.NOT_ENOUGH_PRODUCT_QUANTITY.getMessage());
+    }
+}
