6b9cf4f5cf1320a353b52c3bee4a45c3b9e8e954
KJungW <sagakkb0707@naver.com>
2024-11-17 19:12:34 +0900

    test(promotion): 프로모션 미적용 상품 계산 테스트 추가
    테스트 내용
    - 정상적으로 프로모션 미적용 상품을 계산하는지 테스트
    - 프로모션 기간이 아닌 상품은 계산에서 제외하는지 테스트

--- src/test/java/store/service/PromotionServiceTest.java
+++ src/test/java/store/service/PromotionServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import store.domain.BasketItem;
 import store.domain.Product;
 import store.dto.AdditionalGiftItem;
+import store.dto.NonPromotableItem;
 import store.generator.PromotionGenerator;
 import store.repository.BasketItemRepository;
 import store.repository.ProductRepository;
@@ -66,5 +67,32 @@ class PromotionServiceTest {
         List<AdditionalGiftItem> additionalGiftItem = promotionService.findAdditionalGiftItem();
 
         assertThat(additionalGiftItem).isEmpty();
+    }
+
+    @DisplayName("프로모션 미적용 상품을 계산한다")
+    @Test
+    void 프로모션_미적용_상품을_계산한다() {
+        promotionRepository.add(PromotionGenerator.makeValid("특별할인", 2, 1));
+        productRepository.add(new Product("콜라", 1000, 10, 10, "특별할인"));
+        basketItemRepository.add(new BasketItem("콜라", 12));
+
+        List<NonPromotableItem> nonPromotableItems = promotionService.findNonPromotableItem();
+
+        assertThat(nonPromotableItems)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(List.of(new NonPromotableItem("콜라", 3)));
+    }
+
+    @DisplayName("프로모션 기간이 아닌 상품의 경우 프로모션 미적용 상품 계산에서 제외한다")
+    @Test
+    void 프로모션_기간이_아닌_상품의_경우_프로모션_미적용_상품_계산에서_제외한다() {
+        promotionRepository.add(PromotionGenerator.makeInvalid("특별할인", 2, 1));
+        productRepository.add(new Product("콜라", 1000, 10, 10, "특별할인"));
+        basketItemRepository.add(new BasketItem("콜라", 12));
+
+        List<NonPromotableItem> nonPromotableItems = promotionService.findNonPromotableItem();
+
+        assertThat(nonPromotableItems).isEmpty();
     }
 }
